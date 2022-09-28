9026cc73f689d986416a88b56588533974b074b3
devalpi <admin@alpi.dev>
2022-09-28 23:20:10 +0300

    feat: add tags

---
+++ app/Validators/TagRequestValidator.ts
@@ -0,0 +1,10 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+
+export default class TagRequestValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    name: schema.string({ trim: true }),
+  })
+}
