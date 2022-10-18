dabf2e98c5cc91398caf9c630e54b53c9b167abe
devalpi <admin@alpi.dev>
2022-10-18 21:45:37 +0300

    chore: update chapter factory

--- database/factories/ChapterFactory.ts
+++ database/factories/ChapterFactory.ts
@@ -8,7 +8,7 @@ import VolumeFactory from 'Database/factories/VolumeFactory'
 export default Factory.define(Chapter, ({ faker }) => {
   return {
     title: faker.lorem.word(3),
-    number: faker.datatype.number(),
+    number: faker.datatype.number({ min: 1, max: 100 }),
     context: faker.lorem.paragraphs(3),
     translation_note: faker.lorem.paragraphs(1),
     is_mature: faker.datatype.boolean(),
