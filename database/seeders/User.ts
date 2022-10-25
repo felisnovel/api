54080d7384c4e5c54df50eed3f08e72b54a4eb35
devalpi <admin@alpi.dev>
2022-10-26 00:12:14 +0300

    chore: update user seeder

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,21 +1,22 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import User from 'App/Models/User'
 import UserFactory from 'Database/factories/UserFactory'
 
 export default class extends BaseSeeder {
   public async run() {
+    await UserFactory.merge({
+      username: 'lexor',
+      email: 'emre@nerdesin.dev',
+      password: 'secret',
+    })
+      .with('reviews', 2)
+      .with('comments', 3)
+      .with('followNovels', 4)
+      .create()
+
     await UserFactory.merge({ password: 'password' })
       //.with('followNovels', 10)
       //.with('likeNovels', 10)
       //.with('readChapters', 10)
       .createMany(5)
-
-    await User.createMany([
-      {
-        username: 'lexor',
-        email: 'emre@nerdesin.dev',
-        password: 'secret',
-      },
-    ])
   }
 }
