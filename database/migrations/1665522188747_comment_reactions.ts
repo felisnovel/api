27e8d8ee0e5cf90e1a83a0c1ef2eb7c818c461c1
devalpi <admin@alpi.dev>
2022-10-27 22:11:41 +0300

    fix: add not nullable to attributes for comment reactions table

--- database/migrations/1665522188747_comment_reactions.ts
+++ database/migrations/1665522188747_comment_reactions.ts
@@ -7,10 +7,10 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.integer('user_id').unsigned()
+      table.integer('user_id').unsigned().notNullable()
       table.foreign('user_id').references('users.id')
 
-      table.integer('comment_id').unsigned()
+      table.integer('comment_id').unsigned().notNullable()
       table.foreign('comment_id').references('comments.id')
 
       table.enum('type', ['like', 'dislike']).notNullable()
