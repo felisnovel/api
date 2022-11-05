f97955c7debe6cc48908100b8b8826f8821307ac
devalpi <admin@alpi.dev>
2022-11-05 23:22:25 +0300

    fix: comments query without parent id

--- app/Controllers/Http/CommentController.ts
+++ app/Controllers/Http/CommentController.ts
@@ -23,6 +23,8 @@ export default class CommentController {
 
     if (request.input('parent_id')) {
       commentsQuery.where('parent_id', request.input('parent_id'))
+    } else {
+      commentsQuery.whereNull('parent_id')
     }
 
     const comments = await commentsQuery
