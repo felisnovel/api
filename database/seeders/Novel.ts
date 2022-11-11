71c3b59bd31778b446cb3dd1556f2040e20b6733
devalpi <admin@alpi.dev>
2022-11-11 22:56:49 +0300

    chore: update novel seeder

--- database/seeders/Novel.ts
+++ database/seeders/Novel.ts
@@ -1,29 +1,22 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import Novel from 'App/Models/Novel'
 import ChapterFactory from 'Database/factories/ChapterFactory'
 import NovelFactory from 'Database/factories/NovelFactory'
 
 export default class extends BaseSeeder {
   public async run() {
-    await NovelFactory.with('user', 1)
+    const novels = await NovelFactory.with('user', 1)
+      .with('volumes', 1)
       .with('tags', 2)
       .merge({
         is_promoted: true,
       })
-      .createMany(10)
-
-    const novels = await Novel.all()
+      .createMany(22)
 
     for (const novel of novels) {
       await ChapterFactory.merge({
         novel_id: novel.id,
-      })
-        .with('volume', 1, (volumeFactory) =>
-          volumeFactory.merge({
-            volume_novel_id: novel.id,
-          })
-        )
-        .createMany(10)
+        volume_id: novel.volumes[0].id,
+      }).createMany(25)
     }
   }
 }
