b9482474d9f128d21f2dc25c5f17904bf2fc177b
devalpi <admin@alpi.dev>
2022-12-08 21:12:53 +0300

    chore: update reports

--- app/Controllers/Http/Report/CoinReport.ts
+++ app/Controllers/Http/Report/CoinReport.ts
@@ -1,22 +1,41 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import _ from 'lodash'
 import { DateTime } from 'luxon'
-import { getCalendarDatesForRange, getWeekForDay } from '../../../../utils'
 import OrderType from '../../../Enums/OrderType'
 import Order from '../../../Models/Order'
 
 export default class CoinReport {
-  async invoke({}: HttpContextContract) {
-    const initialDate = DateTime.local()
-    const selectedWeek = getWeekForDay(initialDate)
-    const dates = getCalendarDatesForRange(selectedWeek)
+  async invoke({ request }: HttpContextContract) {
+    const startDate = request.input('startDate')
+      ? DateTime.fromFormat(request.input('startDate'), 'yyyy/MM/dd')
+      : DateTime.local().minus({ weeks: 1 })
+    const endDate = DateTime.fromFormat(
+      request.input('endDate', DateTime.local().toFormat('yyyy/MM/dd')),
+      'yyyy/MM/dd'
+    )
 
-    const orders = await Order.query().where('type', OrderType.COIN).preload('packet')
+    const dates = []
+
+    if (startDate <= endDate) {
+      let currentDate = startDate
+
+      while (currentDate <= endDate) {
+        dates.push(currentDate)
+        currentDate = currentDate.plus({ days: 1 })
+      }
+    }
+
+    const orders = await Order.query()
+      .where('type', OrderType.COIN)
+      .preload('packet')
+      .whereBetween('created_at', [
+        startDate.minus({ days: 1 }).toFormat('yyyy-MM-dd'),
+        endDate.plus({ days: 1 }).toFormat('yyyy-MM-dd'),
+      ])
 
     const series = _.chain(orders)
       .groupBy('packet.name')
       .map((packetOrders, packetName) => {
-        console.log(packetOrders[0].amount)
         return {
           name: packetName || 'Belirsiz',
           data: dates.map((date) => ({
@@ -36,22 +55,28 @@ export default class CoinReport {
         .reduce((total, item) => parseFloat(item.amount) + total, 0),
     }))
 
-    return [
-      {
-        shape: 'Bar',
-        name: 'Paket Satışları',
-        series: series,
-      },
-      {
-        shape: 'Line',
-        name: 'Toplam Satışlar',
-        series: [
+    return {
+      chart: {
+        type: 'combo',
+        title: 'Coin/Paket Satışları',
+        data: [
           {
-            name: 'Coin Miktarı',
-            data: totalOrders,
+            shape: 'Bar',
+            name: 'Paket Satışları',
+            series: series,
+          },
+          {
+            shape: 'Line',
+            name: 'Toplam Satışlar',
+            series: [
+              {
+                name: 'Coin Miktarı',
+                data: totalOrders,
+              },
+            ],
           },
         ],
       },
-    ]
+    }
   }
 }
--- app/Controllers/Http/Report/ReadReport.ts
+++ app/Controllers/Http/Report/ReadReport.ts
@@ -2,13 +2,27 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import _ from 'lodash'
 import { DateTime } from 'luxon'
-import { getCalendarDatesForRange, getWeekForDay } from '../../../../utils'
 
 export default class ReadReport {
-  async invoke({}: HttpContextContract) {
-    const initialDate = DateTime.local()
-    const selectedWeek = getWeekForDay(initialDate)
-    const dates = getCalendarDatesForRange(selectedWeek)
+  async invoke({ request }: HttpContextContract) {
+    const startDate = request.input('startDate')
+      ? DateTime.fromFormat(request.input('startDate'), 'yyyy/MM/dd')
+      : DateTime.local().minus({ weeks: 1 })
+    const endDate = DateTime.fromFormat(
+      request.input('endDate', DateTime.local().toFormat('yyyy/MM/dd')),
+      'yyyy/MM/dd'
+    )
+
+    const dates = []
+
+    if (startDate <= endDate) {
+      let currentDate = startDate
+
+      while (currentDate <= endDate) {
+        dates.push(currentDate)
+        currentDate = currentDate.plus({ days: 1 })
+      }
+    }
 
     const chapterReads = await Database.query()
       .from('chapter_read')
@@ -16,6 +30,10 @@ export default class ReadReport {
       .leftJoin('novels', 'chapters.novel_id', 'novels.id')
       .whereNotNull('chapter_read.order_id')
       .count('chapter_read.chapter_id as read_count')
+      .whereBetween('chapter_read.created_at', [
+        startDate.minus({ days: 1 }).toFormat('yyyy-MM-dd'),
+        endDate.plus({ days: 1 }).toFormat('yyyy-MM-dd'),
+      ])
       .select(
         'novels.name as novel_name',
         Database.raw("date_trunc('day', chapter_read.created_at) as date")
@@ -41,6 +59,30 @@ export default class ReadReport {
       })
       .value()
 
-    return series
+    return {
+      chart: {
+        type: 'line',
+        title: 'Premium Bölüm Okunmaları',
+        data: series,
+      },
+      table: {
+        columns: [
+          {
+            Header: 'Seri Adı',
+            accessor: 'novel_name',
+          },
+          {
+            Header: 'Okunma Sayısı',
+            accessor: 'read_count',
+          },
+        ],
+        data: series
+          .map((x) => ({
+            novel_name: x.name,
+            read_count: x.data.reduce((total, item) => item.value + total, 0),
+          }))
+          .sort((a, b) => b.read_count - a.read_count),
+      },
+    }
   }
 }
