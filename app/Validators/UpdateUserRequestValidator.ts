99d05aa8d983e0c9a88a1bd3ae82e9e2a86ca960
devalpi <admin@alpi.dev>
2022-12-23 22:20:28 +0300

    fix: update password regex

--- app/Validators/UpdateUserRequestValidator.ts
+++ app/Validators/UpdateUserRequestValidator.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { rules, schema } from '@ioc:Adonis/Core/Validator'
 import UserGender from 'App/Enums/UserGender'
+import { PASSWORD_REGEX } from '../constants/Regex'
 
 export default class UpdateUserRequestValidator {
   constructor(protected ctx: HttpContextContract) {}
@@ -10,9 +11,10 @@ export default class UpdateUserRequestValidator {
     email: schema.string.optional({ trim: true }),
     bio: schema.string.optional({ trim: true }),
     gender: schema.enum.optional(Object.values(UserGender)),
-    password: schema.string.optional({ trim: true }, [
-      rules.maxLength(64),
-      rules.confirmed('password_confirmation'),
+    password: schema.string.optional({}, [
+      rules.minLength(8),
+      rules.regex(PASSWORD_REGEX),
+      rules.confirmed(),
     ]),
     facebook_handle: schema.string.optional({ trim: true }),
     twitter_handle: schema.string.optional({ trim: true }),
