4570056e90ccd1fff8ef248bd63ef373148b61a7
devalpi <admin@alpi.dev>
2022-12-27 23:29:52 +0300

    chore: update messages to base validator

--- app/Validators/BaseValidator.ts
+++ app/Validators/BaseValidator.ts
@@ -1,12 +1,28 @@
 import { CustomMessages } from '@ioc:Adonis/Core/Validator'
-
+const PASSWORD_REGEX_MESSAGE = 'Parola en az 1 büyük, 1 küçük, 1 rakam, 1 özel karakter içermelidir'
 export default class BaseValidator {
   public messages: CustomMessages = {
-    enum: 'The value must be one of {{ options.choices }}',
-    minLength: '{{ field }} must be at least {{ options.minLength }} characters long',
-    maxLength: '{{ field }} must be less then {{ options.maxLength }} characters long',
-    required: '{{ field }} alanı zorunludur',
-    unique: '{{ field }} daha önce kullanılmış',
-    exists: '{{ field }} adresi sistemde kayıtlı değil',
+    'enum': '{{ options.choices }} seçeneklerinden birini seçmelisiniz.',
+    'minLength': '{{ field }} en az {{ options.minLength }} karakter olmalıdır.',
+    'maxLength': '{{ field }} en fazla {{ options.maxLength }} karakter olmalıdır.',
+    'required': '{{ field }} alanı zorunludur',
+    'unique': '{{ field }} daha önce kullanılmış',
+    'exists': '{{ field }} adresi sistemde kayıtlı değil',
+    'alpha': '{{ field }} sadece harflerden oluşmalıdır.',
+    'alphaNum': '{{ field }} sadece harfler ve rakamlar içermelidir.',
+    'array': '{{ field }} dizi olmalıdır.',
+    'boolean': '{{ field }} sadece doğru veya yanlış olmalıdır.',
+    'confirmed': '{{ field }} tekrarı eşleşmiyor.',
+    'distinct': '{{ field }} alanı yinelenen bir değere sahip.',
+    'email': '{{ field }} alanına girilen e-posta adresi geçersiz.',
+    'file': '{{ field }} dosya olmalıdır.',
+    'ip': '{{ field }} geçerli bir IP adresi olmalıdır.',
+    'notIn': 'Seçili {{ field }} geçersiz.',
+    'regex': '{{ field }} biçimi geçersiz.',
+    'url': '{{ field }} biçimi geçersiz.',
+
+    'password.regex': PASSWORD_REGEX_MESSAGE,
+    '_password.regex': PASSWORD_REGEX_MESSAGE,
+    'username.regex': 'Kullanıcı adı sadece harf ve rakam içermelidir',
   }
 }
