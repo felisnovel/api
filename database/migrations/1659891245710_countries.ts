0d64c46dfa3fa940c1d5734624af3e28ebb5c7d6
devalpi <admin@alpi.dev>
2023-02-08 01:55:50 +0300

    chore: rename countries migration

--- database/migrations/1659891245710_countries.ts
+++ database/migrations/1659891245710_countries.ts
@@ -9,12 +9,6 @@ export default class extends BaseSchema {
 
       table.string('name')
       table.string('key').unique()
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
     })
   }
 
