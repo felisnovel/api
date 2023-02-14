8a3e5e93abb89b0a0a0c9d21879cc6fb4b1b6c59
devalpi <admin@alpi.dev>
2023-02-15 00:45:58 +0300

    chore: remove nullable invoice_id, order_id for invoice order table

--- database/migrations/1675806857498_invoice_order.ts
+++ database/migrations/1675806857498_invoice_order.ts
@@ -7,10 +7,10 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.integer('invoice_id').nullable().unsigned()
+      table.integer('invoice_id').unsigned()
       table.foreign('invoice_id').references('invoices.id').onDelete('CASCADE')
 
-      table.integer('order_id').nullable().unsigned()
+      table.integer('order_id').unsigned()
       table.foreign('order_id').references('orders.id').onDelete('CASCADE')
     })
   }
