e66e32c8a6d8aceaf4ff5591546f3b01c9c34927
devalpi <admin@alpi.dev>
2023-03-19 16:51:11 +0300

    feat: add swagger

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,16 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from 'Config/swagger'
+
+Route.get('/swagger', async () => {
+  return AutoSwagger.docs(Route.toJSON(), swagger)
+})
+
+Route.get('/docs', async () => {
+  return AutoSwagger.ui('/swagger')
+})
+
 Route.group(() => {
   Route.post('/email-confirmation', 'User/EmailConfirmationController.send')
 }).middleware('auth')
